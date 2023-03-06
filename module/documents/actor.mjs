21fdeaa865bb7f8c6c9edcd143b8914b4d25d519
Lefteris Vladimiros <spirit@siradio.fm>
2023-03-06 16:15:11 +0200

    systemData charstat

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -23,40 +23,42 @@ export class MetanthropesActor extends Actor {
 	// progressed (spent xp to increase)
 	// base (initial + progressed)
 	// charstat (characteristic + stat base totals)
+	// I will need to calculate the charstat in prepareBaseData as it's value will be used for effects calculations
 
-	///prepareBaseData() {
+	/** @override */
 	prepareBaseData() {
 		console.log("Metanthropes RPG starting prepareBaseData");
-		for (let ch of Object.values(this.characteristics)) {
-			ch.base = Math.sum(ch.initial, ch.progressed);
+		const actorData = this;
+		const systemData = actorData.system;
+		for (let [key, ch] of Object.entries(systemData.characteristics)) {
+			console.log(ch.label);
+			console.log(ch.initial.label);
+			console.log(ch.initial.value);
+			console.log(ch.progressed.label);
+			console.log(ch.progressed.value);
+			ch.base = (ch.initial.value + ch.progressed.value);
 			console.log(ch.base);
-			for (let st of Object.values(this.stats.ch.characteristics)) {
-				st.base = Math.sum(st.initial, st.progressed);
-				st.charstat = Math.sum(ch.base, st.base);
+			for (let [key, st] of Object.entries(systemData.stats.ch.characteristics)) {
+				st.base = (st.initial + st.progressed);
+				st.charstat = (ch.base + st.base);
+				console.log(st.label);
+				console.log(st.initial);
+				console.log(st.progressed);
 				console.log(st.base);
 				console.log(st.charstat);
 			}
 		}
 	}
 
-
 	/**
-		 * @override
-		 * Augment the basic actor data with additional dynamic data. Typically,
-		 * you'll want to handle most of your calculated/derived data in this step.
-		 * Data calculated in this step should generally not exist in template.json
-		 * (such as ability modifiers rather than ability scores) and should be
-		 * available both inside and outside of character sheets (such as if an actor
-		 * is queried and has a roll executed directly from it).
-		 * qp notes
-		 * ==========
-		 * this is where we add the body etc to the stats
-		 * here we should also add the modifiers see above
-		 * copilot go?
-		 * 
-		 * todo enw edw tha einai as poyme to base + conditions + buffs
-		 * 
-		 */
+	 * @override
+	 * Augment the basic actor data with additional dynamic data. Typically,
+	 * you'll want to handle most of your calculated/derived data in this step.
+	 * Data calculated in this step should generally not exist in template.json
+	 * (such as ability modifiers rather than ability scores) and should be
+	 * available both inside and outside of character sheets (such as if an actor
+	 * is queried and has a roll executed directly from it).
+	 */
 	prepareDerivedData() {
 		const actorData = this;
 		const systemData = actorData.system;
@@ -71,71 +73,100 @@ export class MetanthropesActor extends Actor {
 		this._prepareMetaTherionData(actorData);
 		this._prepareAnimalData(actorData);
 		this._prepareArtificialData(actorData);
-		//todo animated objects, humanoids, vehicles, etc
+		this._prepareAnimatedObjectData(actorData);
+		this._prepareAnimatedHumanoidData(actorData);
+		this._prepareVehicleData(actorData);
+
+	}
+	_prepareHumanoidData(actorData) {
+		if (actorData.type !== 'Humanoid') return;
+		const systemData = actorData.system;
+	}
+	_prepareMetanthropeData(actorData) {
+		if (actorData.type !== 'Metanthrope') return;
+		const systemData = actorData.system;
+	}
+	_prepareProtagonistData(actorData) {
+		if (actorData.type !== 'Protagonist') return;
+		const systemData = actorData.system;
 	}
 
+	_prepareMetaTherionData(actorData) {
+		if (actorData.type !== 'MetaTherion') return;
+		const systemData = actorData.system;
+	}
 
-	//todo the below for all the others
-	_prepareProtagonistData(actorData) {
-	if (actorData.type !== 'Protagonist') return;
-	const systemData = actorData.system;
-}
+	_prepareAnimalData(actorData) {
+		if (actorData.type !== 'Animal') return;
+		const systemData = actorData.system;
+	}
 
-_prepareHumanoidData(actorData) {
-	if (actorData.type !== 'Humanoid') return;
-	const systemData = actorData.system;
-	//if (actorData.type !== 'Metanthrope') return;
-	//if (actorData.type !== 'Animal') return;
-	//if (actorData.type !== 'Artificial') return;
-	//if (actorData.type !== 'MetaTherion') return;
+	_prepareArtificialData(actorData) {
+		if (actorData.type !== 'Artificial') return;
+		const systemData = actorData.system;
+	}
+
+	_prepareAnimatedObjectData(actorData) {
+		if (actorData.type !== 'Animated Object') return;
+		const systemData = actorData.system;
+	}
+
+	_prepareAnimatedHumanoidData(actorData) {
+		if (actorData.type !== 'Animated Humanoid') return;
+		const systemData = actorData.system;
+	}
+
+	_prepareVehicleData(actorData) {
+		if (actorData.type !== 'Vehicle') return;
+		const systemData = actorData.system;
+	}
 	// should I nest these --- together? what does this do exactly?
 	// Make modifications to data here. For example:
 	//systemData.xp = (systemData.cr * systemData.cr) * 100;
-}
 
 
 
-getRollData() {
-	const data = super.getRollData();
+	getRollData() {
+		const data = super.getRollData();
 
-	// Prepare character roll data.
-	//todo: qp preparing for template switch
-	this._prepareHumanoidRollData(data);
-	//this._prepareMetanthropeRollData(data);
-	this._prepareProtagonistRollData(data);
-	//this._prepareMetaTherionRollData(data);
-	//this._prepareAnimalRollData(data);
-	//this._prepareArtificialRollData(data);
+		// Prepare character roll data.
+		//todo: qp preparing for template switch
+		this._prepareHumanoidRollData(data);
+		//this._prepareMetanthropeRollData(data);
+		this._prepareProtagonistRollData(data);
+		//this._prepareMetaTherionRollData(data);
+		//this._prepareAnimalRollData(data);
+		//this._prepareArtificialRollData(data);
 
-	//todo: I need to do the _getCharacterRollData changes below
-	this._getProtagonistRollData(data);
-	this._getHumanoidRollData(data);
+		//todo: I need to do the _getCharacterRollData changes below
+		this._getProtagonistRollData(data);
+		this._getHumanoidRollData(data);
 
-	return data;
-}
+		return data;
+	}
 
-_getCharacterRollData(data) {
-	if (this.type !== 'Protagonist') return;
+	_getCharacterRollData(data) {
+		if (this.type !== 'Protagonist') return;
 
-	// Copy the ability scores to the top level, so that rolls can use
-	// formulas like `@str.mod + 4`.
-	//todo: qp preparing for template switch - or not? maybe this section is not needed????
-	//? is this where the numbering bug is coming from?
-	//! test this
-	//if (data.stats) {
-	//	for (let [k, v] of Object.entries(data.stats)) {
-	//		data[k] = foundry.utils.deepClone(v);
-	//	}
-}
+		// Copy the ability scores to the top level, so that rolls can use
+		// formulas like `@str.mod + 4`.
+		//todo: qp preparing for template switch - or not? maybe this section is not needed????
+		//? is this where the numbering bug is coming from?
+		//! test this
+		//if (data.stats) {
+		//	for (let [k, v] of Object.entries(data.stats)) {
+		//		data[k] = foundry.utils.deepClone(v);
+		//	}
+	}
 
-// Add level for easier access, or fall back to 0.
-// if (data.attributes.level) {
-// 	data.lvl = data.attributes.level.value ?? 0;
-// }
-_getHumanoidRollData(data) {
-	if (this.type !== 'Humanoid') return;
+	// Add level for easier access, or fall back to 0.
+	// if (data.attributes.level) {
+	// 	data.lvl = data.attributes.level.value ?? 0;
+	// }
+	_getHumanoidRollData(data) {
+		if (this.type !== 'Humanoid') return;
 
-	// Process additional NPC data here.
-}
+		// Process additional NPC data here.
+	}
 
 }
