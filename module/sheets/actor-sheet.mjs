efa63fc4c8faf3203322f069a7806613ea32a932
Lefteris Vladimiros <spirit@siradio.fm>
2023-03-25 14:33:13 +0200

    possessions groundwork

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -87,7 +87,7 @@ export class MetanthropesActorSheet extends ActorSheet {
 			}
 			// Append to Metapowers.
 			else if (i.type === "Metapower") {
-				if (i.system.level != undefined) {
+				if (i.system.Level.value != undefined) {
 					Metapowers[i.system.Level.value].push(i);
 				}
 			} else if (i.type === "Combo") {
