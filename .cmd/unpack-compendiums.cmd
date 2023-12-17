b3308c0fc91ac77cee06973f5a9e985331a1ed8b
Lefteris Vladimiros <spirit@siradio.fm>
2023-12-17 18:55:20 +0200

    Compendiums Built via CLI

---
+++ .cmd/unpack-compendiums.cmd
@@ -0,0 +1,16 @@
+// Run this in a new Git cmd window when you have made changes to Compendiums within FoundryVTT
+// Select this code and right click on the Git cmd window to paste and press enter
+// Once complete, you can close the Git cmd window
+//todo: use recursion and make it so that it's the same for modules and systems
+npm install -g @foundryvtt/foundryvtt-cli
+fvtt configure set installPath "F:/FoundryVTT/Foundry Virtual Tabletop"
+fvtt configure set dataPath "F:/FoundryVTT/"
+fvtt package unpack "introductory-scenes" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/introductory-scenes"
+fvtt package unpack "how-to-play" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/how-to-play"
+fvtt package unpack "narrator-toolkit" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/narrator-toolkit"
+fvtt package unpack "metapowers" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/metapowers"
+fvtt package unpack "narrator-notes" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/narrator-notes"
+fvtt package unpack "possessions" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/possessions"
+fvtt package unpack "premade-protagonists" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/premade-protagonists"
+fvtt package unpack "premade-antagonists" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/premade-antagonists"
+fvtt package unpack "rollable-tables" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/rollable-tables"
