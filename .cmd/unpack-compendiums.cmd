ab2b143e9ed168075a4f9f214f249f530542e105
Lefteris Vladimiros <spirit@siradio.fm>
2023-12-17 23:32:52 +0200

    Modular System v1

--- .cmd/unpack-compendiums.cmd
+++ .cmd/unpack-compendiums.cmd
@@ -1,10 +1,12 @@
 // Run this in a new Git cmd window when you have made changes to Compendiums within FoundryVTT
-// Select this code and right click on the Git cmd window to paste and press enter
-// Once complete, you can close the Git cmd window
+// Make sure FoundryVTT Electron app is closed before running this
+// Select these commands, CTRL+C, and then right click on the Git cmd window, to paste them and press enter
+// This should take about a minute and once complete, you can close the Git cmd window
 //todo: use recursion and make it so that it's the same for modules and systems
 npm install -g @foundryvtt/foundryvtt-cli
 fvtt configure set installPath "F:/FoundryVTT/Foundry Virtual Tabletop"
 fvtt configure set dataPath "F:/FoundryVTT/"
+fvtt package workon "metanthropes-system" --type "System"
 fvtt package unpack "introductory-scenes" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/introductory-scenes"
 fvtt package unpack "how-to-play" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/how-to-play"
 fvtt package unpack "narrator-toolkit" --out "F:/FoundryVTT/Data/systems/metanthropes-system/src/packs/narrator-toolkit"
