ee6578b1922f088e9ec693a947e9d65a174acbe2
Lefteris Vladimiros <spirit@siradio.fm>
2024-05-26 21:49:32 +0300

    Early Access Release build 0.9.589

--- module/metanthropes/actorcontrol.mjs
+++
@@ -1,6 +0,0 @@
-
-//* Give Control of an Actor to a Player, to allow showing the chat buttons for rerolls and execution
-export async function metaActorControl(actor) {
-	const playerName = game.user.name;
-	await actor.update({ "system.metaowner.value": playerName });
-}
