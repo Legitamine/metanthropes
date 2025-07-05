d5e844b6a00353a9b0c37f120943aa80ba401f76
bofhgr <qp.wormholes@gmail.com>
2025-07-05 18:23:35 +0300

    fix(ui): remove custom color

--- module/ui/sheets/actor-sheet.mjs
+++ module/ui/sheets/actor-sheet.mjs
@@ -88,8 +88,6 @@ export class MetanthropesActorSheet extends foundry.appv1.sheets.ActorSheet {
 		context.isNarrator = game.user.isGM;
 		//? Add the actor's active effects to the context for easier access.
 		if (context.betaTesting) context.effects = metanthropes.utils.prepareActiveEffectCategories(this.actor.effects);
-		//? Custom FA Icon Color
-		context.customFAColor = await game.settings.get("metanthropes", "metaCustomFAColor");
 		//? Calculate the actor's XP Spent
 		context.xpSpent = Number(actorData.system.Vital.Experience.Spent + actorData.system.Vital.Experience.Manual);
 		//? Flag if actor is affected by Disease
