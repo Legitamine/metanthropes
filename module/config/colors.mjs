91a1ae6bb57ae9150375917c268f9d3c2b51d7f4
bofhgr <qp.wormholes@gmail.com>
2025-08-12 23:16:23 +0300

    feat(ui): Second pass at consolidating/refactoring scss.

--- module/config/colors.mjs
+++ module/config/colors.mjs
@@ -1,9 +1,9 @@
 export const colors = Object.freeze({
-	primary: "#9A5D9B",
-	secondary: "#0CA79F",
-	highlight: "#EC008C",
+	primary: "#00b0bc",
+	secondary: "#945d90",
+	highlight: "#e6007e",
 	buff: "#267114",
 	condition: "#ab1616",
-	success: "#4CAF50",
-	failure: "#E57373",
+	success: "#267114",
+	failure: "#ab1616",
 });
