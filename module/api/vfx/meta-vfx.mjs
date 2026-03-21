93c8679be17d5e540222875ff84ffc17ba89dfe9
qp <qp.wormholes@gmail.com>
2026-03-21 23:17:39 +0200

    feat(vfx): more VFX testing & dummy assets

--- module/api/vfx/meta-vfx.mjs
+++ module/api/vfx/meta-vfx.mjs
@@ -12,8 +12,8 @@ export async function metaVFX(actor, manuallySelectedTargets, effect, elementalD
 	//!xreiazomai ta tokens apo to game.user.targets oxi ta token documents
 	const mL = metanthropes.utils.metaLog;
 	mL(3, "metaVFX", "VFX");
-	//? Grab the initiating actor's token document
-	const actorToken = await actor.getActiveTokens(false, true)[0];
+	//? Grab the initiating actor's Token
+	const actorToken = await actor.getActiveTokens(false, false)[0];
 	if (!actorToken) return mL(5, "metaVFX", "No Initiating Actor Token could be found for actor", actor);
 	mL(3, "metaVFX", "actorToken", actorToken, "manuallySelectedTargets", manuallySelectedTargets);
 
@@ -21,6 +21,7 @@ export async function metaVFX(actor, manuallySelectedTargets, effect, elementalD
 	// based off the properties of the Item that was used. Would need to create a 'master' animation with all component combinations and test that
 	// can we run multiple VFXEffects in parallel? How does clone affect us? Need to decide/define the master combinations
 	// Should also think about VFXEffects not triggered by metaExecute, but rather on other triggers like Criticals / Hunger / Cover, Bloodsplats etc
+	// Also, we should be able to call a specific effect (like textscroll for other things too like when losing life (vs damage dealt) and others (portal opens, trap triggered!))
 
 	//* VFX Tests
 	const shot = new foundry.canvas.vfx.VFXEffect({
@@ -98,7 +99,6 @@ export async function metaVFX(actor, manuallySelectedTargets, effect, elementalD
 			{ component: "damage", position: 200 },
 		],
 	});
-
 	const splatter = new foundry.canvas.vfx.VFXEffect({
 		name: "bloodSplatter",
 		components: {
@@ -116,115 +116,6 @@ export async function metaVFX(actor, manuallySelectedTargets, effect, elementalD
 		},
 		timeline: [{ component: "splash" }],
 	});
-	const metaVFX = new foundry.canvas.vfx.VFXEffect({
-		//todo improvements: speed & overlap, random rotation, fades, scaling via token size or life total?, delta positions?
-		name: "metaVFX",
-		components: {
-			// flight: {
-			// 	type: "singleAttack",
-			// 	path: [
-			// 		{ reference: "origin", deltas: { sort: 1 } },
-			// 		{ reference: "target", deltas: { sort: 1 } },
-			// 	],
-			// 	pathType: "arc",
-			// 	charge: {
-			// 		texture: "systems/metanthropes/assets/artwork/vfx/particle-1.webp",
-			// 		duration: 300,
-			// 		animations: [{ function: "drawBack", params: {} }],
-			// 		//sound: { src: "ogg", align : 2}
-			// 	},
-			// 	projectile: {
-			// 		texture: "systems/metanthropes/assets/artwork/vfx/particle-2.webp",
-			// 		speed: 6, // feet per second;  duration computed from path length //? 32f/s is 10m/s
-			// 		size: { w: 48, h: 24 }, //number in feet ?
-			// 		animations: [{ function: "followPath", params: {} }],
-			// 		sound: {
-			// 			src: "systems/metanthropes/assets/audio/sfx/Astral_Cue_Metal_Detector_01.ogg",
-			// 			align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START, //align: foundry.canvas.sfx.SOUND_ALIGNMENT.START,
-			// 		},
-			// 	},
-			// 	projectile: {
-			// 		texture: "systems/metanthropes/assets/artwork/vfx/particle-4.webp",
-			// 		speed: 12, // feet per second;  duration computed from path length //? 32f/s is 10m/s
-			// 		size: { w: 48, h: 24 }, //number in feet ?
-			// 		animations: [{ function: "followPath", params: {} }],
-			// 		sound: {
-			// 			src: "systems/metanthropes/assets/audio/sfx/Aether_Cue_Brain_electric.ogg",
-			// 			align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START, //align: foundry.canvas.sfx.SOUND_ALIGNMENT.START,
-			// 		},
-			// 	},
-			// 	impact: {
-			// 		//bloodsplatter?
-			// 		texture: "systems/metanthropes/assets/artwork/vfx/particle-3.webp",
-			// 		duration: 400,
-			// 		sound: {
-			// 			src: "systems/metanthropes/assets/audio/sfx/Astral_Cue_Modern_device_beeping_01.ogg",
-			// 			align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
-			// 		},
-			// 	},
-			// },
-			boom1: {
-				type: "singleImpact",
-				position: { reference: "target", deltas: { sort: 0 } }, //what does this sort affect?
-				texture: "systems/metanthropes/assets/artwork/vfx/boom-1.png",
-				size: 10,
-				animations: [{ function: "scale", params: {} }],
-				duration: 230,
-			},
-			boom2: {
-				type: "singleImpact",
-				position: { reference: "target", deltas: { sort: 1 } },
-				texture: "systems/metanthropes/assets/artwork/vfx/boom-2.png",
-				size: 10,
-				animations: [{ function: "scale", params: {} }],
-				duration: 230,
-			},
-			boom3: {
-				type: "singleImpact",
-				position: { reference: "target", deltas: { sort: 0 } },
-				texture: "systems/metanthropes/assets/artwork/vfx/boom-3.png",
-				size: 10,
-				animations: [{ function: "scale", params: {} }],
-				duration: 230,
-			},
-			boom4: {
-				type: "singleImpact",
-				position: { reference: "target", deltas: { sort: 1 } },
-				texture: "systems/metanthropes/assets/artwork/vfx/boom-4.png",
-				size: 10,
-				animations: [{ function: "scale", params: {} }],
-				duration: 230,
-			},
-			boom5: {
-				type: "singleImpact",
-				position: { reference: "target", deltas: { sort: 0 } },
-				texture: "systems/metanthropes/assets/artwork/vfx/boom-5.png",
-				size: 10,
-				animations: [{ function: "scale", params: {} }],
-				duration: 230,
-			},
-			text: {
-				type: "scrollingText",
-				//origin: { reference: "target", property: "center" }, //origin is confusing here, why not position?
-				origin: { reference: "target" },
-				//content: { reference: "text" },
-				content: `${elementalDamageRollResult} Elemental`,
-				duration: 2500, //optional def 2000
-				scrollDirection: CONST.TEXT_ANCHOR_POINTS.TOP,
-				textAnchor: CONST.TEXT_ANCHOR_POINTS.CENTER, //optional
-				textStyle: { fill: "#d21414", fontSize: 28, fontWeight: "bold" },
-			},
-		},
-		timeline: [
-			//{ component: "flight", position: 0 },
-			{ component: "text", position: 0 },
-			{ component: "boom1", position: 100 },
-			{ component: "boom2", position: 180 },
-			{ component: "boom3", position: 380 },
-			{ component: "boom4", position: 580 },
-			{ component: "boom5", position: 780 },
-		],
-	});
 	const dmgText = new foundry.canvas.vfx.VFXEffect({
 		name: "DmgText",
 		components: {
@@ -240,70 +131,199 @@ export async function metaVFX(actor, manuallySelectedTargets, effect, elementalD
 		},
 		timeline: [{ component: "damage", position: 0 }],
 	});
-	//* We need to iterate for each token from the manuallySelectedTargets
-	//? Resolve each targeted actor's token document from the array so we can use them for this effect
+	const metaVFX = new foundry.canvas.vfx.VFXEffect({
+		//todo improvements: speed & overlap, random rotation, fades, scaling via token size or life total?, delta positions?
+		name: "metaVFX",
+		components: {
+			//?the type of the component matters, not the key name
+			
+			kame: {
+				type: "singleAttack",
+				path: [
+					{ reference: "origin", deltas: { sort: 1 } },
+					{ reference: "target", deltas: { sort: 1 } },
+				],
+				pathType: "arc",
+				charge: {
+					texture: "systems/metanthropes/assets/artwork/possessions/fist.svg",
+					duration: 1500,
+					animations: [{ function: "drawBack" }],
+					sound: {
+						src: "systems/metanthropes/assets/audio/sfx/reload-heavy-03.wav",
+						align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					},
+				},
+				projectile: {
+					texture: "systems/metanthropes/assets/artwork/vfx/particle-2.webp",
+					// size: { w: 48, h: 24 }, //number in feet ?
+					size: 3, // feet
+					speed: 24, // feet per second;  duration computed from path length //? 32f/s is 10m/s
+					animations: [{ function: "followPath" }],
+					sound: {
+						src: "systems/metanthropes/assets/audio/sfx/astral-cue-metal-detector-01.ogg",
+						align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START, //align: foundry.canvas.sfx.SOUND_ALIGNMENT.START,
+					},
+				},
+				impact: {
+					//bloodsplatter?
+					texture: "systems/metanthropes/assets/artwork/vfx/particle-3.webp",
+					duration: 2000,
+					sound: {
+						src: "systems/metanthropes/assets/audio/sfx/distant-explosion-03.wav",
+						align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					},
+				},
+			},
+			boom1: {
+				type: "singleImpact",
+				position: { reference: "target", deltas: { sort: 0 } }, //timeline position? what does this sort affect?
+				texture: "systems/metanthropes/assets/artwork/vfx/boom-1.png",
+				size: 10,
+				animations: [{ function: "scale", params: {} }],
+				duration: 230,
+				sound: {
+					src: "systems/metanthropes/assets/audio/sfx/small-explosion-01.wav",
+					align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					radius: 120, //def 60 - what is this, feet?
+				},
+			},
+			boom2: {
+				type: "singleImpact",
+				position: { reference: "target", deltas: { sort: 1 } },
+				texture: "systems/metanthropes/assets/artwork/vfx/boom-2.png",
+				size: 10,
+				animations: [{ function: "scale", params: {} }],
+				duration: 230,
+				sound: {
+					src: "systems/metanthropes/assets/audio/sfx/small-explosion-02.wav",
+					align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					radius: 120, //def 60 - what is this, feet?
+				},
+				shake: {
+					type: "shake",
+					target: "stage", //can also target individual canvas layers or groups
+					duration: 200,
+					maxDisplacement: 20, //35 def
+					smoothness: 0.6,
+				},
+			},
+			boom3: {
+				type: "singleImpact",
+				position: { reference: "target", deltas: { sort: 0 } },
+				texture: "systems/metanthropes/assets/artwork/vfx/boom-3.png",
+				size: 10,
+				animations: [{ function: "scale", params: {} }],
+				duration: 230,
+				sound: {
+					src: "systems/metanthropes/assets/audio/sfx/small-explosion-03.wav",
+					align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					radius: 120, //def 60 - what is this, feet?
+				},
+			},
+			boom4: {
+				type: "singleImpact",
+				position: { reference: "target", deltas: { sort: 1 } },
+				texture: "systems/metanthropes/assets/artwork/vfx/boom-4.png",
+				size: 10,
+				animations: [{ function: "scale", params: {} }],
+				duration: 230,
+				sound: {
+					src: "systems/metanthropes/assets/audio/sfx/small-explosion-04.wav",
+					align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					radius: 120, //def 60 - what is this, feet?
+				},
+			},
+			boom5: {
+				type: "singleImpact",
+				position: { reference: "target", deltas: { sort: 0 } },
+				texture: "systems/metanthropes/assets/artwork/vfx/boom-5.png",
+				size: 10,
+				animations: [{ function: "scale", params: {} }],
+				duration: 230,
+				sound: {
+					src: "systems/metanthropes/assets/audio/sfx/small-explosion-05.wav",
+					align: foundry.canvas.vfx.constants.SOUND_ALIGNMENT.START,
+					radius: 120, //def 60 - what is this, feet?
+				},
+			},
+			particles: {
+				type: "particleGenerator",
+				textures: [
+					"systems/metanthropes/assets/artwork/vfx/particle-1.webp",
+					"systems/metanthropes/assets/artwork/vfx/particle-2.webp",
+					"systems/metanthropes/assets/artwork/vfx/particle-3.webp",
+					"systems/metanthropes/assets/artwork/vfx/particle-4.webp",
+				],
+				area: { reference: "target" },
+				count: 40,
+				duration: 2500,
+				lifetime: { min: 300, max: 700 },
+				fade: { in: 50, out: 200 },
+				scale: { min: 0.1, max: 0.4 },
+				velocity: { speed: 1, angle: 1 },
+				config: {
+					constraints: { mode: "none" },
+					drift: { enabled: true, intensity: 0.8 },
+				},
+			},
+			text: {
+				type: "scrollingText",
+				//content: "${elementalDamageRollResult} Elemental",
+				content: { reference: "text" },
+				//distance: 5, // no idea, no default
+				duration: 5000, //optional def 2000
+				jitter: 10, //def 0
+				//origin: { reference: "target", property: "center" }, //origin is confusing here, why not position?
+				origin: { reference: "target" },
+				scrollDirection: CONST.TEXT_ANCHOR_POINTS.TOP,
+				textAnchor: CONST.TEXT_ANCHOR_POINTS.TOP, //optional
+				textStyle: { fill: "#d21414", fontSize: 28, fontWeight: "bold" },
+			},
+		},
+		timeline: [
+			{ component: "kame", position: 0 }, //can I have relative positions in the timeline - how do you know how much time it takes for the projectile to reach the target?
+			{ component: "boom1", position: 3100 },
+			{ component: "boom2", position: 3180 },
+			{ component: "boom3", position: 3380 },
+			{ component: "boom4", position: 3580 },
+			{ component: "boom5", position: 3780 },
+			{ component: "particles", position: 3840 },
+			{ component: "text", position: 5000 },
+		],
+	});
+
+	//* Go thru each Token from the manuallySelectedTargets - future input from Targeting validation
 	for (const targetedActor of manuallySelectedTargets) {
-		//const targetToken = await fromUuid(targetedActor); //todo is Sync here required?
-		//const target = game.user.targets.first();
-		//mL(3, "metaVFX", "Shot VFX", "Initiating Actor Token", actorToken, "Target Token", target);
-		//todo check if shot.started = true; then clone
-		//await shot.play({
-		// origin: {...actorToken.center, elevation: actorToken.document.elevation, sort: actorToken.document.sort},
-		// target: {...targetToken.center, elevation: targetToken.document.elevation, sort: targetToken.document.sort},
-		//origin: { ...actorToken.center },
-		//target: { ...targetToken.center },
-		//});
-		//mL(3, "metaVFX", "Impact VFX");
-		// await impact.play({
-		// 	target: targetToken,
-		// 	damageText: `${elementalDamageRollResult} Elemental`,
-		// });
-		//mL(3, "metaVFX", "Splatter VFX");
-		// splatter.play({
-		// 	// target: { ...targetToken.center, elevation: targetToken.document.elevation, sort: targetToken.document.sort },
-		// 	// target: { ...targetToken.center, elevation: targetToken.document.elevation, sort: targetToken.document.sort },
-		// 	target: { ...target.center, elevation: target.document.elevation, sort: target.document.sort },
-		// });
 		mL(3, "metaVFX", "Target's Name", targetedActor.name);
-		// if (dmgText.started) {
-		// 	const newVFX = dmgText.clone();
-		// 	await newVFX.play({
-		// 		target: targetedActor.center,
-		// 		damageText: `${elementalDamageRollResult} Elemental`,
-		// 	});
-		// } else {
-		// 	await dmgText.play({
-		// 		target: targetedActor.center,
-		// 		damageText: `${elementalDamageRollResult} Elemental`,
-		// 	});
-		// }
+		//! if we await the VFX then it goes from one target to the next, if we don't, it does all at the same time.
 		if (metaVFX.started) {
+			mL(3, "metaVFX", "Cloning VFX");
 			const newVFX = metaVFX.clone();
-			await newVFX.play({
+			newVFX.play({
 				origin: {
 					...actorToken.center,
-					//elevation: actorToken.document.elevation,
-					//sort: actorToken.document.sort,
+					elevation: actorToken.document.elevation,
+					sort: actorToken.document.sort,
 				},
 				target: {
 					...targetedActor.center,
-					//elevation: targetedActor.document.elevation,
-					//sort: targetedActor.document.sort,
+					elevation: targetedActor.document.elevation,
+					sort: targetedActor.document.sort,
 				},
 				text: "Elemental",
 			});
 			continue;
 		}
-		await metaVFX.play({
+		metaVFX.play({
 			origin: {
 				...actorToken.center,
-				//elevation: actorToken.document.elevation,
-				//sort: actorToken.document.sort,
+				elevation: actorToken.document.elevation,
+				sort: actorToken.document.sort,
 			},
 			target: {
 				...targetedActor.center,
-				//elevation: targetedActor.document.elevation,
-				//sort: targetedActor.document.sort, //?sort controls if the effect will be under or over the token (enabled is over)
+				elevation: targetedActor.document.elevation,
+				sort: targetedActor.document.sort, //?sort controls if the effect will be under or over the token (enabled is over)
 			},
 			text: `${elementalDamageRollResult} Elemental`,
 		});
